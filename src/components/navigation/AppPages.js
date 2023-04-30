2ca663fc79e8b358a763c8a02b4b68c599435b33
Asala El Ghandour <asala.elghandour@gmail.com>
2023-04-30 21:58:24 +0300

    making the sidebar responsive on window resizing

--- src/components/navigation/AppPages.js
+++ src/components/navigation/AppPages.js
@@ -1,8 +1,6 @@
-import Navbar from './Navbar';
-import SideBar from './SideBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
-import { BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
+import { Route } from "react-router-dom";
 import InspectionSchedule from '../../layout/InspectionSchedule';
 import LeavesRequests from '../../layout/LeavesRequests';
 import AttendanceRecord from '../../layout/AttendanceRecord';
@@ -12,20 +10,18 @@ import FuelAndOdometerData from '../../layout/FuelAndOdometerData';
 import AddUser from '../../layout/AddUser';
 import AddVehicle from '../../layout/AddVehicle';
 import Dashboard from '../../layout/Dashboard';
-import React, {useState} from 'react';
-import MovementPlan from '../../layout/MovementPlan';
 
-export default function AppPages(){
-  return(    <>    
-          <Route exact path="/OdoMotoReactFrontend" component={Dashboard}/>
-          <Route exact path="/OdoMotoReactFrontend/inspection" component={InspectionSchedule}/>
-          <Route exact path="/OdoMotoReactFrontend/leaves_requests" component={LeavesRequests}/>
-          <Route exact path="/OdoMotoReactFrontend/attendance_record" component={AttendanceRecord}/>
-          <Route exact path="/OdoMotoReactFrontend/vehicles_list" component={Vehicles}/>
-          <Route exact path="/OdoMotoReactFrontend/drivers_list" component={DriverList}/>
-          <Route exact path="/OdoMotoReactFrontend/fuel_and_odometer_data" component={FuelAndOdometerData}/>
-          <Route exact path="/OdoMotoReactFrontend/add_user" component={AddUser}/>
-          <Route exact path="/OdoMotoReactFrontend/add_vehicle" component={AddVehicle}/>
-</>
+export default function AppPages() {
+  return (<>
+    <Route exact path="/OdoMotoReactFrontend" component={Dashboard} />
+    <Route exact path="/OdoMotoReactFrontend/inspection" component={InspectionSchedule} />
+    <Route exact path="/OdoMotoReactFrontend/leaves_requests" component={LeavesRequests} />
+    <Route exact path="/OdoMotoReactFrontend/attendance_record" component={AttendanceRecord} />
+    <Route exact path="/OdoMotoReactFrontend/vehicles_list" component={Vehicles} />
+    <Route exact path="/OdoMotoReactFrontend/drivers_list" component={DriverList} />
+    <Route exact path="/OdoMotoReactFrontend/fuel_and_odometer_data" component={FuelAndOdometerData} />
+    <Route exact path="/OdoMotoReactFrontend/add_user" component={AddUser} />
+    <Route exact path="/OdoMotoReactFrontend/add_vehicle" component={AddVehicle} />
+  </>
   );
 }
--- src/components/navigation/SideBar.js
+++ src/components/navigation/SideBar.js
@@ -1,90 +1,94 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SvgComponent from './logo';
 import { Link } from 'react-router-dom';
 
 export default function SideBar() {
+    const mediaQuery = window.matchMedia("(min-width: 42rem)");
+    const [query, setQuery] = useState(mediaQuery);
+
+    React.useEffect(() => {
+        mediaQuery.addEventListener("change", (mquery) => {
+            setQuery(mquery);
+        });
+        return () => mediaQuery.removeEventListener("change", (mquery) => { setQuery(mquery) });
+    }, [mediaQuery]);
+
+    const [expanded, setExpanded] = React.useState(query.matches);
+    useEffect(() => {
+        setExpanded(query.matches);
+    }, [query]);
     return (
-        <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion collapse show" id="accordionSidebar">
-            <Link className="sidebar-brand d-flex align-items-center justify-content-center" to="/">
-                <SvgComponent style={{marginTop: 40, marginBottom:5}}/>
-            </Link>
-
-            <hr className="sidebar-divider my-0 mt-5"/>
-
-            <li className="nav-item active">
-                <Link className="nav-link" to="/">
-                    <i className="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span>
+        expanded ?
+            (<ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion collapse show" id="accordionSidebar" >
+                <Link className="sidebar-brand d-flex align-items-center justify-content-center" to="/">
+                    <SvgComponent style={{ marginTop: 40, marginBottom: 5 }} />
                 </Link>
-            </li>
-
-            <hr className="sidebar-divider"/>
-
-            <div className="sidebar-heading">
-                Interface
-            </div>
-
-            <li className="nav-item">
-                <Link className="nav-link collapsed" to="#" data-toggle="collapse" data-target="#collapseTwo"
-                    aria-expanded="true" aria-controls="collapseTwo">
-                    <i className="fas fa-fw fa-calendar-alt"></i>
-                    <span>Attendance and leaves</span>
-                </Link>
-                <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div className="bg-white py-2 collapse-inner rounded">
-                        <h6 className="collapse-header">Attendance and leaves:</h6>
-                        <Link className="collapse-item" to="/attendance_record">Attendance Records</Link>
-                        <Link className="collapse-item" to="/leaves_requests">Leaves Requests</Link>
-                    </div>
+                <hr className="sidebar-divider my-0 mt-5" />
+                <li className="nav-item active">
+                    <Link className="nav-link" to="/">
+                        <i className="fas fa-fw fa-tachometer-alt"></i>
+                        <span>Dashboard</span>
+                    </Link>
+                </li>
+                <hr className="sidebar-divider" />
+                <div className="sidebar-heading">
+                    Interface
                 </div>
-            </li>
-
-            <li className="nav-item">
-                <Link className="nav-link collapsed" to="#" data-toggle="collapse" data-target="#collapseUtilities"
-                    aria-expanded="true" aria-controls="collapseUtilities">
-                    <i className="fas fa-fw fa-briefcase"></i>
-                    <span>Users</span>
-                </Link>
-                <div id="collapseUtilities" className="collapse" aria-labelledby="headingUtilities"
-                    data-parent="#accordionSidebar">
-                    <div className="bg-white py-2 collapse-inner rounded">
-                        <h6 className="collapse-header">Users</h6>
-                        
-                        <Link className="collapse-item" to="/add_user">Add User</Link>
-                        <Link className="collapse-item" to="/users_list">Users List</Link>
-                        <Link className="collapse-item" to="/drivers_list">Drivers List</Link>
+                <li className="nav-item">
+                    <Link className="nav-link collapsed" to="#" data-toggle="collapse" data-target="#collapseTwo"
+                        aria-expanded="true" aria-controls="collapseTwo">
+                        <i className="fas fa-fw fa-calendar-alt"></i>
+                        <span>Attendance and leaves</span>
+                    </Link>
+                    <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                        <div className="bg-white py-2 collapse-inner rounded">
+                            <h6 className="collapse-header">Attendance and leaves:</h6>
+                            <Link className="collapse-item" to="/attendance_record">Attendance Records</Link>
+                            <Link className="collapse-item" to="/leaves_requests">Leaves Requests</Link>
+                        </div>
                     </div>
-                </div>
-            </li>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link collapsed" to="#" data-toggle="collapse" data-target="#collapseUtilities"
+                        aria-expanded="true" aria-controls="collapseUtilities">
+                        <i className="fas fa-fw fa-briefcase"></i>
+                        <span>Users</span>
+                    </Link>
+                    <div id="collapseUtilities" className="collapse" aria-labelledby="headingUtilities"
+                        data-parent="#accordionSidebar">
+                        <div className="bg-white py-2 collapse-inner rounded">
+                            <h6 className="collapse-header">Users</h6>
 
-            <li className="nav-item">
-                <Link className="nav-link collapsed" to="#" data-toggle="collapse" data-target="#collapsePages"
-                    aria-expanded="true" aria-controls="collapsePages">
-                    <i className="fas fa-fw fa-car"></i>
-                    <span>Vehicles</span>
-                </Link>
-                <div id="collapsePages" className="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                    <div className="bg-white py-2 collapse-inner rounded">
-                        <h6 className="collapse-header">Vehicles</h6>
-                        <Link className="collapse-item" to="/add_vehicle">Add Vehicle</Link>
-                        <Link className="collapse-item" to="/vehicles_list">Vehicles List</Link>
-                        <Link className="collapse-item" to="/inspection">Inspection Schedule</Link>
-                        <Link className="collapse-item" to="/fuel_and_odometer_data">Fuel and Odometer Records</Link>
+                            <Link className="collapse-item" to="/add_user">Add User</Link>
+                            <Link className="collapse-item" to="/users_list">Users List</Link>
+                            <Link className="collapse-item" to="/drivers_list">Drivers List</Link>
+                        </div>
                     </div>
-                </div>
-            </li>
-
-            <li className="nav-item">
-                <Link className="nav-link" to="/movement_plan">
-                    <i className="fas fa-fw fa-table"></i>
-                    <span>Generate Movement Plan</span></Link>
-            </li>
-
-            <hr className="sidebar-divider d-none d-md-block"/>
-
-            {/* <div className="text-center d-none d-md-inline">
-                <button className="rounded-circle border-0" id="sidebarToggle"></button>
-            </div> */}
-        </ul>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link collapsed" to="#" data-toggle="collapse" data-target="#collapsePages"
+                        aria-expanded="true" aria-controls="collapsePages">
+                        <i className="fas fa-fw fa-car"></i>
+                        <span>Vehicles</span>
+                    </Link>
+                    <div id="collapsePages" className="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                        <div className="bg-white py-2 collapse-inner rounded">
+                            <h6 className="collapse-header">Vehicles</h6>
+                            <Link className="collapse-item" to="/add_vehicle">Add Vehicle</Link>
+                            <Link className="collapse-item" to="/vehicles_list">Vehicles List</Link>
+                            <Link className="collapse-item" to="/inspection">Inspection Schedule</Link>
+                            <Link className="collapse-item" to="/fuel_and_odometer_data">Fuel and Odometer Records</Link>
+                        </div>
+                    </div>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/movement_plan">
+                        <i className="fas fa-fw fa-table"></i>
+                        <span>Generate Movement Plan</span></Link>
+                </li>
+                <hr className="sidebar-divider d-none d-md-block" />
+            </ul>
+            )
+            : null
     );
 }
